7e679b771e6cfe91d43a60b2c09720568d1ca35e
dandelion <dandelionpost279@gmail.com>
2025-10-01 11:40:05 +0530

    Add working GoogleTest with headless Chrome k CI setup

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -1,15 +1,29 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
-	@Test
-	public void openGoogle() {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		System.out.println("Title is:" + driver.getTitle());
-		driver.quit();
-	
-	}
+
+    @Test
+    public void openGoogle() {
+        // Setup ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Run Chrome in headless mode for CI/CD
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.google.com");
+        System.out.println("Title is: " + driver.getTitle());
+
+        driver.quit();
+    }
 }
